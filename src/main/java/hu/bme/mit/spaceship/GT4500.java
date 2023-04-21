5ae0d424f51be49c746e30e96c560f0a877b8b07
Sámuel Csányi <samuel.csanyi@gmail.com>
2023-04-21 13:21:48 +0200

    implemented the GT4500 problem

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,9 +77,8 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
-
+        if (! secondaryTorpedoStore.isEmpty() && !primaryTorpedoStore.isEmpty())
+            firingSuccess = secondaryTorpedoStore.fire(1) &&  primaryTorpedoStore.fire(1);
         break;
     }
 
